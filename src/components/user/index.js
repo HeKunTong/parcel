fb3bbb03957b553369dff208d1766227aca273d9
blank <1161709455@qq,com>
2018-12-24 17:45:27 +0800

    个人信息

--- src/components/user/index.js
+++ src/components/user/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CyTabs from './cyTabs';
 import UserInfo from './userInfo';
+import CySex from './cySex';
 import CyTask from './cyTask';
 import CyNotice from './cyNotice';
 import CyFeedback from './cyFeedback';
@@ -12,7 +13,8 @@ class User extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedKey: "my"
+            selectedKey: "my",
+            show: false
         };
     }
 
@@ -22,89 +24,29 @@ class User extends Component {
 
     render() {
 
-        const { selectedKey } = this.state;
+        const { selectedKey, show } = this.state;
 
         return (
             <div className="module-cy-user-page module-cy-user-page-mine" style={{height: 969, right: 0}}>
                 <div className="cy-user-page-main">
                     {
-                        selectedKey === "my" &&
-                        <UserInfo />
+                        selectedKey === "my" && <UserInfo onChange={this.onChange}/>
                     }
                     {
-                        selectedKey === "task" &&
-                        <CyTask />
+                        selectedKey === "task" && <CyTask />
                     }
                     {
-                        selectedKey === "notice" &&
-                        <CyNotice/>
+                        selectedKey === "notice" && <CyNotice/>
                     }
                     {
-                        selectedKey === "feedback" &&
-                        <CyFeedback />
+                        selectedKey === "feedback" && <CyFeedback />
                     }
                     {
-                        selectedKey === "set" &&
-                        <CySet/>
+                        selectedKey === "set" && <CySet/>
                     }
-                    <div className="module-sex" style={{display: "none"}}>
-                        <div className="sex-mask">
-                        </div>
-                        <div/>
-                        <div className="first-come" style={{visibility: "hidden"}}>
-                        </div>
-                        <div className="sex-config">
-                            <div className="sex-title">
-                                修改信息
-                            </div>
-                            <div className="sex-center">
-                                <div className="sex-header">
-                                    <div className="sex-select">
-                                        <div className="i-am">
-                                            <span className="text">我是：</span>
-                                            <span className="boy select" value="1">
-                                        <span className="icon-boy"/>男
-                                        </span>
-                                            <span className="girl" value="0">
-                                        <span className="icon-girl"/>女
-                                        </span>
-                                        </div>
-                                        <div className="i-also-am icon-man">
-                                            <span className="text">也是：</span>
-                                            <span className="icon" value="2000">
-                                            <span className="age-icon icon-00"/>
-                                            <span className="icon-text">00后</span>
-                                        </span>
-                                            <span className="icon" value="1990">
-                                            <span className="age-icon icon-90"/>
-                                            <span className="icon-text">90后</span>
-                                        </span>
-                                            <span className="icon select" value="1980">
-                                            <span className="age-icon icon-80"/>
-                                            <span className="icon-text">80后</span>
-                                        </span>
-                                            <span className="icon" value="1970">
-                                            <span className="age-icon icon-70"/>
-                                            <span className="icon-text">70后</span>
-                                        </span>
-                                            <span className="icon" value="1960">
-                                            <span className="age-icon icon-60"/>
-                                            <span className="icon-text">60后</span>
-                                        </span>
-                                            <span className="icon" value="1950">
-                                            <span className="age-icon icon-50"/>
-                                            <span className="icon-text">50后</span>
-                                        </span>
-                                        </div>
-                                        <div className="alldown">
-                                            <span className="down-button">完成</span>
-                                        </div>
-                                        <div className="otherword">让小伙伴们更加了解你吧</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        show && <CySex onChange={this.onChange}/>
+                    }
                 </div>
                 <CyTabs selectedKey={selectedKey} onChange={this.onChange}/>
                 <CyClose />
--- src/components/user/userInfo.js
+++ src/components/user/userInfo.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 
 class UserInfo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            editShow: false,
+            tabKey: "comment"
+        };
+    }
+
+    onEditShow = () => {
+        this.setState({editShow: true});
+    }
+
+    onEditUnShow = () => {
+        this.setState({editShow: false});
+    }
+
+    onShow = () => {
+        const {  onChange } = this.props;
+        onChange({show: true});
+    }
+
+    onChangeTab = (tabKey) => {
+        this.setState({ tabKey });
+    }
+
     render() {
+
+        const { editShow, tabKey } = this.state;
+
         return (
-            <div className="module-cy-user-info" style={{display: "block"}}>
+            <div className="module-cy-user-info">
                 <div className="cy-user-info-header clear-g">
                     <div className="cy-user-photo-container">
                         <img className="cy-user-photo" src="http://sucimg.itc.cn/avatarimg/10106078459859_1545038887446_c55"/>
@@ -11,61 +40,70 @@ class UserInfo extends Component {
                     </div>
                     <div className="cy-user-info">
                         <div className="cy-user-info-txt">
-                            <em className="cy-user-name">blank</em>
-                            <input className="cy-user-name-input" type="text"/>
-                            <span className="cy-user-edit"/>
-                            <div className="cy-user-edit-btn">确定</div>
+                            { !editShow &&  <em className="cy-user-name">blank</em> }
+                            { !editShow &&  <span className="cy-user-edit" onClick={this.onEditShow}/> }
+                            { editShow && <input className="cy-user-name-input" type="text"/> }
+                            { editShow && <div className="cy-user-edit-btn" onClick={this.onEditUnShow}>确定</div> }
                         </div>
                         <div className="cy-user-sex">
                             <span className="cy-user-sex-gender cy-user-sex-gender-1-age-1980"/>
                             <span className="cy-user-sex-age">80后·男</span>
-                            <span className="cy-user-sex-update">点击修改</span>
+                            <span className="cy-user-sex-update" onClick={this.onShow}>点击修改</span>
                         </div>
                     </div>
                 </div>
                 <div className="cy-user-my">
                     <ul className="cy-my-label clear-g">
-                        <li className="active">
+                        <li className={tabKey === "comment" ? "active" : ""} onClick={() => this.onChangeTab("comment")}>
                             <div className="cy-my-label-item"><span className="cy-num">0</span> 评论</div>
                         </li>
-                        <li>
+                        <li className={tabKey === "collect" ? "active" : ""} onClick={() => this.onChangeTab("collect")}>
                             <div className="cy-my-label-item" style={{position: "relative"}}><span
                                 className="cy-num">0</span> 收藏
                                 <div className="module-cmt-notice-bubble" style={{display: "none"}}>0</div>
                             </div>
                         </li>
-                        <li>
+                        <li className={tabKey === "reply" ? "active" : ""} onClick={() => this.onChangeTab("reply")}>
                             <div className="cy-my-label-item" style={{position: "relative"}}><span
                                 className="cy-num">0</span> 回复
                                 <div className="module-cmt-notice-bubble" style={{display: "none"}}>0</div>
                             </div>
                         </li>
                     </ul>
-                    <div className="cy-tab-active-bar"/>
+                    <div className="cy-tab-active-bar" style={{marginLeft: tabKey === "comment" ? 63 : tabKey === "collect" ? 273 : 483}}/>
                     <div className="cy-my-page-container">
-                        <div className="cy-my-page cy-my-page-comment active" style={{height: 816}}>
-                            <ul className="cy-my-comment-list">
-                                <div className="empty-hold-place">
-                                    <div className="pet-pic"/>
-                                    <div className="empty-txt comment-empty-txt"/>
-                                    <div className="power-by-cy-txt"/>
-                                </div>
-                            </ul>
-                        </div>
-                        <div className="cy-my-page cy-my-page-collect" style={{height: 816}}>
-                            <ul className="cy-my-comment-list">
+                        {
+                            tabKey === "comment" &&
+                            <div className="cy-my-page cy-my-page-comment" style={{height: 816}}>
+                                <ul className="cy-my-comment-list">
+                                    <div className="empty-hold-place">
+                                        <div className="pet-pic"/>
+                                        <div className="empty-txt comment-empty-txt"/>
+                                        <div className="power-by-cy-txt"/>
+                                    </div>
+                                </ul>
+                            </div>
+                        }
+                        {
+                            tabKey === "collect" &&
+                            <div className="cy-my-page cy-my-page-collect" style={{height: 816}}>
+                                <ul className="cy-my-comment-list">
 
-                            </ul>
-                        </div>
-                        <div className="cy-my-page cy-my-page-reply" style={{height: 816}}>
-                            <ul className="cy-my-comment-list">
-                                <div className="empty-hold-place">
-                                    <div className="pet-pic"/>
-                                    <div className="empty-txt replay-empty-txt"/>
-                                    <div className="power-by-cy-txt"/>
-                                </div>
-                            </ul>
-                        </div>
+                                </ul>
+                            </div>
+                        }
+                        {
+                            tabKey === "reply" &&
+                            <div className="cy-my-page cy-my-page-reply" style={{height: 816}}>
+                                <ul className="cy-my-comment-list">
+                                    <div className="empty-hold-place">
+                                        <div className="pet-pic"/>
+                                        <div className="empty-txt reply-empty-txt"/>
+                                        <div className="power-by-cy-txt"/>
+                                    </div>
+                                </ul>
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
